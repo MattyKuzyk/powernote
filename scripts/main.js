312e56e5634ae36cd61cd14070fbd36898be0a43
Matt <matthew.kuzyk@gmail.com>
2014-11-01 17:59:47 -0400

    Added routes for notebooks and notes

--- scripts/main.js
+++ scripts/main.js
@@ -26,22 +26,61 @@ var insert = function(data) {
 
 
 $("#btn").on('click', function() {
-  
+
 });
 
 $(document).ready(function() {
 
   var app = $.sammy('.nav', function() {
     this.use('Template');
-    this.get('#/', function(context) {
-      this.load('data/notebooks.json')
-        .then(function(items) {
-            $.each(items, function(i, item) {
-              context.render('templates/notebook-title.template', {item: item})
-               .appendTo(context.$element('.sections'));
-      });
-        });
+
+    this.around(function(callback) {
+        var context = this;
+        this.load('data/notebooks.json')
+            .then(function(notebooks) {
+              context.notebooks = notebooks;
+            })
+            .then(callback);
     });
+
+    this.get('#/', function(context) {
+      $.each(context.notebooks, function(i, notebook) {
+        context.render('templates/notebook-title.template', {notebook: notebook})
+         .appendTo(context.$element('.sections'));
+      });
+    });
+
+    this.get('#/notebooks/:id', function(context) {
+      // context.$element('.pages').not(':first').remove();
+
+      $.each(context.notebooks, function(i, notebook) {
+
+        if (notebook['id'] === context.params['id']) {
+          context.current_notebook = notebook;
+          return false;
+        }
+
+      });
+
+      if (!context.current_notebook) { return context.notFound(); }
+
+      this.load('data/notes.json')
+        .then(function(notes) {
+            $.each(context.current_notebook['page_ids'], function(i, note_id) {
+
+              $.each(notes, function(i, note) {
+                if (note.id === note_id) {
+                context.render('templates/notes-title.template', {note: note})
+               .appendTo(context.$element('.pages'));
+                context.log(note.title, '-', note.artist);
+                }
+            });
+          });
+        });
+
+
+    });
+
   });
 
   app.run('#/');
