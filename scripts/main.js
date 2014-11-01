8f42c53a1c9c002b772511242d40e7a7f106639d
Kartik Talwar <hi@kartikt.com>
2014-11-01 16:20:55 -0400

    Dummy data

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
+var insert = function(data) {
+    var caretPos = document.getElementById("editor").selectionStart;
+    var textAreaTxt = $("#editor").val();
+    var txtToAdd = data;
+    $("#editor").val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+}
+
+$("#btn").on('click', function() {
+  
+});
+
 $(document).ready(function() {
 
   var app = $.sammy('.nav', function() {
