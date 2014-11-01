2c3094173446d047c3a45466b5daad1bb72ef680
Matt <matthew.kuzyk@gmail.com>
2014-11-01 15:35:10 -0400

    Added Sammy.js, updated index name

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 $(document).ready(function() {
+
+  var app = $.sammy('.nav', function() {
+    this.get('#/', function(context) {
+      $.ajax({
+        url: 'http://powernote.cloudapp.net/data/notebooks.json',
+        dataType: 'json',
+        crossDomain: true,
+        success: function(notebooks) {
+          $.each(notebooks, function(i, notebook) {
+            context.log(notebook.title, '-', notebook.description);
+          });
+        }
+      });
+    });
+  });
+
+  app.run('#/');
+
 });  
